773df63aa1411eedc4ef8fa64944bb1328ff070d
Francisco Caballero <fjcero@gmail.com>
2018-10-20 13:52:20 -0300

    Calendar helper and basic styling

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,8 +1,36 @@
 import React from 'react';
 
+import { getMonth } from '../helpers';
+
+const month = getMonth();
+
+const CalendarMonth = ({ month }) => (
+  <div className="calendar__month">
+    {month.map((week, i) => (
+      <div className="calendar__row calendar__week" key={i}>
+        {week.map((day, j) => (
+          <div className="calendar__day" key={j}>
+            {day.toDateString()}
+          </div>
+        ))}
+      </div>
+    ))}
+  </div>
+);
+
+// const CalendarWeek = ({  }) => (
+//   <div>
+//   </div>
+// );
+
+// const CalendarDay = ({ day }) => (
+//   <div>
+//   </div>
+// );
+
 const Calendar = () => (
-  <div>
-    <div>
+  <div className="calendar">
+    <div className="calendar__row calendar__header">
       <div>Sun</div>
       <div>Mon</div>
       <div>Tue</div>
@@ -11,6 +39,7 @@ const Calendar = () => (
       <div>Fri</div>
       <div>Sat</div>
     </div>
+    <CalendarMonth month={month} />
   </div>
 );
 
